9d9d22055ade699ee56a5e810384390511e87ddb
JerryTai666 <jerry20211210@gmail.com>
2026-04-30 11:13:02 +0800

    update

--- around-library/data.js
+++ around-library/data.js
@@ -254,8 +254,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": -1.0,
-        "yaw": 0.5,
+        "yaw": -1.0,
+        "pitch": 0.5,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
